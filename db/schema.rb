1880c3c4a143c9ab417c0f19c2092c53c4ac366b
github username <chao.alexander.123@gmail.com>
2018-11-11 08:19:10 -0500

    remove username column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181111041543) do
+ActiveRecord::Schema.define(version: 20181111131809) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
