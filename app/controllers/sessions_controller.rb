c1962efd584b8bd3b6ccbfeb4bdafc54f0bd33f6
github username <chao.alexander.123@gmail.com>
2018-11-11 08:17:41 -0500

    create users and sessions controllers with required routings for login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    login_user(@user)
+    redirect_to root_path
+  end
+
+end
